d8024f98d5443e7ad636a8b6c8b3ffd50dac6273
전민영 <minyoung@jeonmin-yeongui-MacBook.local>
2015-06-12 02:18:37 +0900

    wow

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,13 +7,27 @@ class MainController < ApplicationController
 
   def random
 
-    @picture = Picture.all.sample
   
-    @ai = Picture.all
-    
-    @array = @ai.shuffle[0..4]
+    @double = Array.new
 
-     @array << @picture
+    array1 = ["선사시대", "고구려", "백제", "그냥 신라", "통일 신라", "발해", "고려", "조선초기 회화", "조선 중기의 회화", "조선 후기의 회화", "조선 말기의 회화", "조선시대의 도자, 공예, 건축"]
+    array2 = [0, 3, 11, 15, 20, 24, 25, 34, 45, 55, 78, 89, 103]
+
+    0.upto(array1.length - 1) do |x|
+      @double << [array1[x], Picture.where(:id => array2[x]..array2[x+1])]
+    end
+
+    category = @double.shuffle[0]
+
+    @picture = categoty[1].sample
+
+    @ai = category[1]
+    @array = @ai.shuffle[0..3]
+
+    @array = Picture.all.shuffle[0..2]
+    @array << @picture
+
+    @array.uniq!
 
   end
 
